570b8c1f774f723606afc80d023a479b508f6c04
Will Hua <will@eventus.io>
2024-03-09 14:06:00 -0600

    reproduction

--- src/routes/sverdle/+page.server.ts
+++ src/routes/sverdle/+page.server.ts
@@ -1,6 +1,7 @@
 import { fail } from '@sveltejs/kit';
 import { Game } from './game';
 import type { PageServerLoad, Actions } from './$types';
+import { promptlayer } from 'promptlayer';
 
 export const load = (({ cookies }) => {
 	const game = new Game(cookies.get('sverdle'));
